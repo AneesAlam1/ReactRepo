43096e53594f4eec3970a5073c5d75c23da40160
rahul4code <you@example.com>
2022-11-22 01:48:42 +0530

    Removed prevent Default

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ function App() {
   const [listitem, updatelist] = useState([]);
   const [todo, setTodo] = useState("");
 
-  function AddItem(e) {
-    e.preventDefault();
-    updatelist([...listitem,todo])
-    setTodo("")
+  function AddItem() {
+    if (todo.length > 0) {
+      updatelist([...listitem, todo])
+      setTodo("")
+    } else {
+      alert("Please add ToDo!!")
+    }
   }
 
+  function removeItem(e) {
+    let currentItem = e.target.parentElement.innerText;
+    const filteredItem = listitem.filter(item => {
+      return item !== currentItem
+    })
+    updatelist([...filteredItem])
+  }
 
   return (
     <>
@@ -25,7 +35,6 @@ function App() {
           <div className="col-sm-4 center">
             <h2>To Do App</h2>
             {/* inputfield */}
-            <form>
               <div className="mb-3">
                 <label for="exampleInputEmail1" className="form-label mb-3">Enter List For Rememeber</label>
                 <input type="text" value={todo} className="form-control" id="text" onChange={(e) => { setTodo(e.target.value) }} />
@@ -34,22 +43,24 @@ function App() {
                 </br>
                 <button className="btn btn-success" onClick={(e) => AddItem(e)}>Add</button>
               </div>
-            </form>
             {/* input field close */}
-            <ul className="list-group">
+            <ul className="list-group" style={{ height: "250px", overflowY: "auto" }}>
               {
                 listitem.map(item => {
-                  return <li key={item} className="list-group-item">{item}</li>
+                  return <>
+                    <li className="list-group-item">{item}
+                      <input style={{ float: "right", fontSize: "10px" }}
+                      type="button" className='btn btn-danger' value="X" onClick={(e) => { removeItem(e) }} />
+                    </li>
+                  </>
                 })
-              } 
+              }
             </ul>
           </div>
           <div className="col-sm-4">
             {/* Last Extra div         */}
           </div>
-
         </div>
-
       </div>
     </>
   );
